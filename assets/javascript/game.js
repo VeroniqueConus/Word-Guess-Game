429fe20e54cdad928b23990ba5dc473d13dd90ac
Veronique Conus <veronique@Veroniques-MacBook-Pro.local>
2019-04-30 23:13:23 -0400

    some audio fixes

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,45 +4,43 @@ var guessesLeft = document.getElementById("guesses-left");
 var wonText = document.getElementById("you-won")
 var lostText = document.getElementById("you-lost")
 var placeholder = document.getElementById("")
-var newGameButton = document.getElementById("new-game")
+var startGameButton = document.getElementById("start-game");
+var placeholderContainerElm = document.getElementById("placeholderContainer");
 var alertGameNotRunning = document.getElementById("game-over");
 
 //Define other variables
 var wins = 0;
 var losses= 0;
 var gameOn = false;
-var computerChoice ='';
+var selectedGameWord ='';
 var computerChoicePlaceholder = [];
 var rightGuesses = [];
 var wrongGuesses = [];
+var guessesLeft = 5
 
-var loseSound = new Audio("/assets/audio/whatthehell.mp3");
-var winSound = new Audio("/assets/audio/bigshot.mp3");
-var startGameSound = new Audio ("/assets/audio/letsroll.mp3");
+//Some audio fun
+var loseSound =  new Audio("assets/audio/whatthehell.mp3"); 
+var winSound =  new Audio("assets/audio/bigshot.mp3");
+var startGameSound =  new Audio("assets/audio/letsroll.mp3");
 
-
-var guessesLeft = 10
-
-
-var startGameButton = document.getElementById("startGameButton");
+var startGameButton = document.getElementById("startGameButton"); //when the "start game button" is clicked, the game starts!
 startGameButton.addEventListener('click', function() {
-    newGame();
+    // startGameButton.onplay(function);
+    startGameSound.play();
+    toggleStartGameButton();
+    selectGameWord();
+    createPlaceholder();
+    createGameEventListeners();
+    gameOn = true;
+    guessesLeft = 5;
+    guessedLetters = [];
+    wrongLetters = [];
 });
 
-// Array that lists out all of the answers.
+// Array that lists out all of the possible options
 var allSimpsonsList = ["homer","marge", "rod", "lisa", "bart", "milhouse", "ned", "abe", "selma"];
 
-
-
-// TODO:
-// Note: Specifically chose names were NO letters were repeated because I couldn't figure out how to make the code account for 2 placeholders w/ the same letter that needed to be updated  :(
-// - counters for number of tries.
-// - handle an incorrect letter guess.
-// - know when all letters have been guessed. 
-//WIN!
-
-
-function toggleStartGameButton() {
+function toggleStartGameButton() { //press button to start game and hides it when playing
     if (startGameButton.style.display === "" || startGameButton.style.display === "block") { // shown
         startGameButton.style.display = "none";
     } else {
@@ -50,33 +48,38 @@ function toggleStartGameButton() {
     }
 }
 
-var selectedGameWord;
+var selectedGameWord; //computer randomly picks a word
 function selectGameWord() {
     selectedGameWord = allSimpsonsList[Math.floor(Math.random() * allSimpsonsList.length)];
     console.log("selectedGameWord = ", selectedGameWord);
 }
 
+//need a place to hold the mystery word
 var placeholderContainerElm = document.getElementById("placeholderContainer");
 var underscores = [];
 
+//for every letter you need to guess, here are some blank spaces
 function createPlaceholder() {
     for (var i = 0; i < selectedGameWord.length; i++) {
-        underscores.push("_");
+        underscores.push(" _ ");
     }
 
+    //this puts spaces between the dashes, so you can count the number of spaces (ie letters) to guess
     placeholderContainerElm.innerHTML = underscores.join(" ");
     placeholderContainerElm.style.display = "block";
 }
 
+//is the letter i typed in the mystery word? Gotta create a function that checks.
 function createGameEventListeners() {
     document.body.addEventListener("keyup", function(evt) {
         isSelectedLetterInSelectedGameWord(evt.key);
     });
 }
 
+    //Gotta check and log the guessed letter as good or bad
 function isSelectedLetterInSelectedGameWord(letterPressed) {
     var letterIndex = selectedGameWord.indexOf(letterPressed);
-    if(letterIndex > -1) { // if it is... do this
+    if (letterIndex > -1) { // if it is... do this
         console.log("good");
         addLetterToPlaceholder(letterIndex, letterPressed)
     } else { // else, do that...
@@ -84,6 +87,10 @@ function isSelectedLetterInSelectedGameWord(letterPressed) {
         checkForNoGuesses();
 
         console.log("bad, guessesLeft", guessesLeft);
+
+        guessesLeft.textContent = guessesLeft;   //now, show on DOM 
+        placeholder.textContent = computerChoicePlaceholder.join('');
+        guessedLetters.textContent = wrongGuesses;
     }
 }
 
@@ -95,6 +102,7 @@ function addLetterToPlaceholder(index, letter) {
 
 function checkForWin() {
     if (underscores.indexOf("_") == -1) {
+        winSound.play();
         console.log("WIN");
     } else {
         console.log("keep going (unless tries are all used up)");
@@ -103,87 +111,15 @@ function checkForWin() {
 
 function checkForNoGuesses() {
     if (guessesLeft === 0) {
+        loseSound.play();
         console.log("no more guesses... end game.")
     }
 }
 
-// This function starts the game and readies all the pieces that make up the gameplay.
-function newGame (){
-    toggleStartGameButton();
-    selectGameWord();
-    createPlaceholder();
-    createGameEventListeners();
 
-
-
-
-    // play sound
-    // startGameSound.play();
-
-    // select a random word
-
-
-
-
-
-    
-    gameOn = true;
-    guessesLeft = 10;
-    guessedLetters = [];
-    wrongLetters = [];
-    
-    // computerChoicePlaceholder = [];
-    // letterAlreadyPicked.textContent = "";
-    // alertGameOff.textContent = "";
-
-    // document.onkeyup = function(event) {
-    //     // computer picks a random Simpson
-    //     computerChoice = allSimpsonsList[Math.floor(Math.random() * allSimpsonsList.length)];
-    //     console.log(computerChoice);
-
-    //     // we need to show the EMPTY spaces needed for the word
-    //     for (var i=0; i < computerChoice.length; i++) {
-    //         if (computerChoice[i] === " ") {
-    //             computerWordPlaceholder.push(" ");
-    //         } else {
-    //             computerWordPlaceHolder.push("_");
-    //         }
-    //     }
-
-    //     //now, show on DOM 
-    //     guessesLeft.textContent = guessesLeft;
-    //     placeholder.textContent = computerChoicePlaceholder.join('');
-    //     guessedLetters.textContent = wrongGuesses;
-    // };
-    
-}
-
-
-
-
-//  computer compares the user's guessed letter to the letters in its random choice word computerChoice
-function guessedLetters(letter){
-    console.log(letter);
-
-    if (gameOn === true && guessedLetters.indexOf(letter) === -1) {
-        guessedLetters.push(letter);
-
-        for (var i = 0; i < computerChoice.length; i++) {
-            if (computerChoice[i].toLowerCase () === letter.loLowerCase ()) {
-                computerChoicePlaceholder[i] = computerChoice[i];
-                letterAlreadyPicked.textContent = "";
-
-            }
-        }
-    }
-}
-
-// let's check the user's letter
-function checkIncorrect (letter) {
-    if (computerWordPlaceHolder.indexOf(letter.toLowerCase()) === -1){
-        guessesLeft--;
-        wrongGuesses.push(letter);
-        guessedLettersText.textContent
-
-    }
-}
+// TO DO:
+// Note: Specifically chose names were NO letters were repeated because I couldn't figure out how to make the code account for 2 placeholders w/ the same letter that needed to be updated  :(
+// - counters for number of tries.
+// - handle an incorrect letter guess.
+// - know when all letters have been guessed. 
+//WIN - play sound, show new game button, reset all counters
